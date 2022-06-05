71640df1735d0ae212adaef1486673fac99cf754
Pius Onobhayedo <piosystems@yahoo.co.uk>
2022-06-05 22:56:47 +0100

    dataSource initialized during splash screen

--- App.tsx
+++ App.tsx
@@ -1,33 +1,15 @@
 import 'reflect-metadata';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 //import ExamplesHome from './src/modules/examples/ExamplesHome';
 import TransactionEntryHome from './src/modules/transaction-entries/TransactionEntryHome';
-import useSqliteDataSource from './src/global/datasources/useSqliteDataSource';
-import { DataSource } from 'typeorm';
 import { Text } from '@rneui/base';
+import useCachedResources from './src/global/hooks/useCachedResources';
 
 const App: React.FC = () => {
 
-  //Bring the custom hook we wrote into this Component for use. To be explained in class
-  const sqliteDataSource = useSqliteDataSource();
-
-  //We need to hold the dataSource once opened, so we can pass it to components for connection.
-  const [dataSource, setDataSource] = useState<DataSource | null>(null)
-
-  //declare the function that we will use to get data source from the hook
-  const getDataSource = async () => {
-    try {
-      const dataSource = await sqliteDataSource;
-      setDataSource(dataSource); //success! save in state
-    } catch (error) {
-      setDataSource(null); //problem! set null in state
-    }
-  }
-
-  useEffect(() => {
-    getDataSource(); //to be called only once.
-  }, [])
+  //Using useCachedResources for dataSource loading, while splash screen is on.
+  const { isLoadingComplete, dataSource } = useCachedResources();
 
   //Prepare our conditional display. What we display will depend on whether dataSource is available or not
   const display = () => {
@@ -41,17 +23,22 @@ const App: React.FC = () => {
     } else {
       return (
         <Text>
-          Cannot find data source
+          Cannot get data source
         </Text>
       )
     }
   }
 
-  return (
-    <SafeAreaView style={styles.container}>
-      {display()}
-    </SafeAreaView>
-  );
+  //Check if loading is complete before returning a view
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaView style={styles.container}>
+        {display()}
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
